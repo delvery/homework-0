c858594e40cb207b889b7a67d77d625b4a0d8323
Dudley Elvery <dudley.elvery@gmail.com>
2018-10-28 20:58:40 -0500

    uploading the baseball library loader script

---
+++ linear-regression-loading-teams-library.R
@@ -0,0 +1,6 @@
+library(tidyverse) #load tidyverse for commands i'm used to
+library(dslabs) #load dslabs for similar reasons
+install.packages("Lahman") #install the Lahman package to access the library
+library(Lahman) #load he Lahman library, with Teams data.frame
+ds_theme_set()
+?Teams #call Help on Teams object for information
