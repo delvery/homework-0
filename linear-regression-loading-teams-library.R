109d1cd26a3b7188ee7032a17a81740ab402c977
Dudley Elvery <dudley.elvery@gmail.com>
2018-11-01 21:11:37 -0500

    Update includes creating a linear model for hr/game vs baseonballs/game and runs/game

--- linear-regression-loading-teams-library.R
+++ linear-regression-loading-teams-library.R
@@ -5,3 +5,11 @@ library(Lahman) #load he Lahman library, with Teams data.frame
 ds_theme_set()
 ?Teams #call Help on Teams object for information
 
+# compute the LM model
+lm_model <- Teams %>% 
+  filter(yearID %in% 1961:2001) %>%
+  mutate(HR_per_game = HR / G, R_per_game = R / G, BB_per_game = BB / G) %>%
+  lm(R_per_game ~ BB_per_game + HR_per_game, data = .)
+
+# print the lm_model object
+lm_model
