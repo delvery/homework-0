3c3f004811ebb5ab057c8c0a23250d6e0fee1dba
Dudley Elvery <dudley.elvery@gmail.com>
2018-11-01 21:12:09 -0500

    adds the libraries needs to run the script

--- galtonheights-object-creation.R
+++ galtonheights-object-creation.R
@@ -1,3 +1,5 @@
+library(tidyverse)
+library(dslabs)
 set.seed(1)
 galton_heights <- GaltonFamilies %>%
   filter(gender == "male") %>%
@@ -6,3 +8,9 @@ galton_heights <- GaltonFamilies %>%
   ungroup() %>%
   select(father, childHeight) %>%
   rename(son = childHeight)
+
+beta1 = seq(0, 1, len=nrow(galton_heights))
+results <- data.frame(beta1 = beta1,
+                      rss = sapply(beta1, rss, beta0 = 36))
+results %>% ggplot(aes(beta1, rss)) + geom_line() + 
+  geom_line(aes(beta1, rss), col=2)
